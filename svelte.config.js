cbed079c742c02e6c2c5f05fdf6d511719df8d57
LufixSch <lukas.kaufbeuren@gmail.com>
2026-06-26 18:17:05 +0200

    migrate to SvelteKit 2

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 import { mdsvex } from 'mdsvex';
 import highlighter from './src/lib/util/codeHighlighter.mjs';
